4a71438d91f4443afcedd7fa5988a1747536ad9e
CatalinaBernal06 <37335457+CatalinaBernal06@users.noreply.github.com>
2018-04-26 11:30:39 -0500

    Update graf.py

--- graf.py
+++ graf.py
@@ -1,7 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-dat = np.loadtxt('datos.txt')
+datos = np.loadtxt('datos.txt')
 plt.figure()
-plt.plot(dat[:,0] , dat[:,1])
-plt.show()
+plt.plot(datos[:,0], datos[:,1], label ='valor inicial de u (A = 1.0)')
+plt.plot(datos[:,0], datos[:,2], label ='u (A = 0.5)')
+plt.plot(datos[:,0], datos[:,3], label ='u (A = -0.5)')
+plt.plot(datos[:,0], datos[:,4], label ='u (A = -1.0)')
+plt.xlabel('X : desplazamiento')
+plt.ylabel('Valor de U')
+plt.title('Ecuacion de Onda')
+plt.legend()
+plt.savefig('onda.pdf')
